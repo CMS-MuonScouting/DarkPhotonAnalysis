5d76644de7040a057c2cb80919209fea4841b3e1
Jakob <jakob.salfeld@cern.ch>
2018-10-07 16:34:59 +0200

    next interation cards

--- DimuonAnalysis2017/limits/makeCardsAndWS.C
+++ DimuonAnalysis2017/limits/makeCardsAndWS.C
@@ -57,10 +57,17 @@
 
 using namespace std;
 
-void makeCardsAndWS(){
+void makeCardsAndWStest(){
   
+
+  TH1F* histChi2_A=new TH1F("histChi2_A","histChi2_A",50,0.5,1.5);
+  TH1F* histChi2_B=new TH1F("histChi2_B","histChi2_B",50,0.5,1.5);
+  TH1F* histChi2_C=new TH1F("histChi2_C","histChi2_C",50,0.5,1.5);
+
+
   //WHICH YEAR
-  	TString year="2018";
+  	TString year="2017";
+	TString suff="testbkgnorm";
   //INPUT FILE WITH HISTOGRAMS TO FIT BACKGROUND
   	TFile* file=TFile::Open("/eos/cms/store/group/phys_exotica/darkPhoton/jakob/"+year+"/mergedForCards"+year+".root");
 
@@ -98,7 +105,7 @@ void makeCardsAndWS(){
 	  	mass = mass+(mass*0.01);
 		RooRealVar MH    ("MH"       	, "MH"          , mass);	
 		RooRealVar resA  ("resA"  	, "RFes. unc. " , mass*0.007);//to be checked
-		RooRealVar resB  ("resB"  	, "RFes. unc. " , mass*0.01);//to be checked
+		RooRealVar resB  ("resB"  	, "RFes. unc. " , mass*0.010);//to be checked
 		RooRealVar resC  ("resC"  	, "RFes. unc. " , mass*0.014);// to be checked
 		RooRealVar m2mu  ("m2mu"  	, "m2mu " 	, massLow		, massHigh);
 
@@ -111,26 +118,32 @@ void makeCardsAndWS(){
 		RooDataHist data_obs_CatA("data_obs_CatA", "", RooArgList(m2mu), catA);
 		RooDataHist data_obs_CatB("data_obs_CatB", "", RooArgList(m2mu), catB);
 		RooDataHist data_obs_CatC("data_obs_CatC", "", RooArgList(m2mu), catC);
+		RooRealVar bkg_norm("bkg_norm", "",catA->Integral());
+		RooRealVar bkg_norm_CatB("bkg_norm_CatB", "",catB->Integral());
+		RooRealVar bkg_norm_CatC("bkg_norm_CatC", "",catC->Integral());
 
 		//fit and get the background PDF
 		TF1 *bkfit = new TF1("bkfit","pol3",massLow,massHigh);
 		catA->Fit(bkfit);
-		RooRealVar a0a("a0a","a0a",bkfit->GetParameter(0));//,bkfit->GetParameter(0)*0.1,bkfit->GetParameter(0)*2.) ;
-      		RooRealVar a1a("a1a","a1a",bkfit->GetParameter(1));//,bkfit->GetParameter(1)*0.1,bkfit->GetParameter(1)*2.);
-      		RooRealVar a2a("a2a","a2a",bkfit->GetParameter(2));//,bkfit->GetParameter(2)*0.1,bkfit->GetParameter(2)*2.) ;
-      		RooRealVar a3a("a3a","a3a",bkfit->GetParameter(3));//,bkfit->GetParameter(3)*0.1,bkfit->GetParameter(3)*2.) ;
+		if(i>24. && (i<200. || i>240.)) {histChi2_A->Fill(bkfit->GetChisquare()/bkfit->GetNDF());}
+		RooRealVar a0a("a0a","a0a",bkfit->GetParameter(0),bkfit->GetParameter(0)-bkfit->GetParError(0),bkfit->GetParameter(0)+bkfit->GetParError(0)) ;
+      		RooRealVar a1a("a1a","a1a",bkfit->GetParameter(1),bkfit->GetParameter(1)-bkfit->GetParError(1),bkfit->GetParameter(1)+bkfit->GetParError(1));
+      		RooRealVar a2a("a2a","a2a",bkfit->GetParameter(2),bkfit->GetParameter(2)-bkfit->GetParError(2),bkfit->GetParameter(2)+bkfit->GetParError(2)) ;
+      		RooRealVar a3a("a3a","a3a",bkfit->GetParameter(3),bkfit->GetParameter(3)-bkfit->GetParError(3),bkfit->GetParameter(3)+bkfit->GetParError(3)) ;
 		RooPolynomial bkg_mass_CatA("bkg_mass_CatA","bkg_mass_CatA",m2mu,RooArgList(a0a,a1a,a2a,a3a),0);
 		catB->Fit(bkfit);
-		RooRealVar a0b("a0b","a0b",bkfit->GetParameter(0));//,bkfit->GetParameter(0)*0.1,bkfit->GetParameter(0)*2.) ;
-      		RooRealVar a1b("a1b","a1b",bkfit->GetParameter(1));//,bkfit->GetParameter(1)*0.1,bkfit->GetParameter(1)*2.);
-      		RooRealVar a2b("a2b","a2b",bkfit->GetParameter(2));//,bkfit->GetParameter(2)*0.1,bkfit->GetParameter(2)*2.) ;
-      		RooRealVar a3b("a3b","a3b",bkfit->GetParameter(3));//,bkfit->GetParameter(3)*0.1,bkfit->GetParameter(3)*2.) ;
+		if(i>24. && (i<200. || i>240.)) {histChi2_B->Fill(bkfit->GetChisquare()/bkfit->GetNDF());}
+		RooRealVar a0b("a0b","a0a",bkfit->GetParameter(0),bkfit->GetParameter(0)-bkfit->GetParError(0),bkfit->GetParameter(0)+bkfit->GetParError(0)) ;
+      		RooRealVar a1b("a1b","a1b",bkfit->GetParameter(1),bkfit->GetParameter(1)-bkfit->GetParError(1),bkfit->GetParameter(1)+bkfit->GetParError(1));
+      		RooRealVar a2b("a2b","a2b",bkfit->GetParameter(2),bkfit->GetParameter(2)-bkfit->GetParError(2),bkfit->GetParameter(2)+bkfit->GetParError(2)) ;
+      		RooRealVar a3b("a3b","a3b",bkfit->GetParameter(3),bkfit->GetParameter(3)-bkfit->GetParError(3),bkfit->GetParameter(3)+bkfit->GetParError(3)) ;
 		RooPolynomial bkg_mass_CatB("bkg_mass_CatB","bkg_mass_CatB",m2mu,RooArgList(a0b,a1b,a2b,a3b),0);
 		catC->Fit(bkfit);
-		RooRealVar a0c("a0c","a0c",bkfit->GetParameter(0));//,bkfit->GetParameter(0)*0.1,bkfit->GetParameter(0)*2.) ;
-      		RooRealVar a1c("a1c","a1c",bkfit->GetParameter(1));//,bkfit->GetParameter(1)*0.1,bkfit->GetParameter(1)*2.);
-      		RooRealVar a2c("a2c","a2c",bkfit->GetParameter(2));//,bkfit->GetParameter(2)*0.1,bkfit->GetParameter(2)*2.) ;
-      		RooRealVar a3c("a3c","a3c",bkfit->GetParameter(3));//,bkfit->GetParameter(3)*0.1,bkfit->GetParameter(3)*2.) ;
+		if(i>24. && (i<200. || i>240.)) {histChi2_C->Fill(bkfit->GetChisquare()/bkfit->GetNDF());}
+		RooRealVar a0c("a0c","a0c",bkfit->GetParameter(0),bkfit->GetParameter(0)-bkfit->GetParError(0),bkfit->GetParameter(0)+bkfit->GetParError(0)) ;
+      		RooRealVar a1c("a1c","a1c",bkfit->GetParameter(1),bkfit->GetParameter(1)-bkfit->GetParError(1),bkfit->GetParameter(1)+bkfit->GetParError(1));
+      		RooRealVar a2c("a2c","a2c",bkfit->GetParameter(2),bkfit->GetParameter(2)-bkfit->GetParError(2),bkfit->GetParameter(2)+bkfit->GetParError(2)) ;
+      		RooRealVar a3c("a3c","a3c",bkfit->GetParameter(3),bkfit->GetParameter(3)-bkfit->GetParError(3),bkfit->GetParameter(3)+bkfit->GetParError(3)) ;
 		RooPolynomial bkg_mass_CatC("bkg_mass_CatC","bkg_mass_CatC",m2mu,RooArgList(a0c,a1c,a2c,a3c),0);
 		
 		//save into ROO workspace
@@ -144,42 +157,61 @@ void makeCardsAndWS(){
       		dpworkspace.import(bkg_mass_CatA);
 		dpworkspace.import(bkg_mass_CatB);
 		dpworkspace.import(bkg_mass_CatC);
-      		dpworkspace.writeToFile(Form("dpWorkspace"+year+"_%d.root",i));
+		//dpworkspace.import(bkg_norm);
+		//dpworkspace.import(bkg_norm_CatB);
+		//dpworkspace.import(bkg_norm_CatC);
+      		dpworkspace.writeToFile(Form("dpWorkspace"+year+suff+"_%d.root",i));
 
 		//write the datacard
 		char inputShape[200];
-      		sprintf(inputShape,"dpCard%d.txt",i);
+      		sprintf(inputShape,"dpCard_"+year+suff+"_%d.txt",i);
       		ofstream newcardShape;
       		newcardShape.open(inputShape);
       		newcardShape << Form("imax 3 number of channels\n");
       		newcardShape << Form("jmax * number of background\n");
       		newcardShape << Form("kmax * number of nuisance parameters\n");
-		newcardShape << Form("shapes data_obs 		CatA dpWorkspace"+year+"_%d.root dpworkspace:data_obs_CatA	\n",i);
-		newcardShape << Form("shapes bkg_mass 		CatA dpWorkspace"+year+"_%d.root dpworkspace:bkg_mass_CatA	\n",i);
-		newcardShape << Form("shapes signalModel 	CatA dpWorkspace"+year+"_%d.root dpworkspace:signalModel_CatA	\n",i);
-		newcardShape << Form("shapes data_obs 		CatB dpWorkspace"+year+"_%d.root dpworkspace:data_obs_CatB	\n",i);
-		newcardShape << Form("shapes bkg_mass 		CatB dpWorkspace"+year+"_%d.root dpworkspace:bkg_mass_CatB	\n",i);
-		newcardShape << Form("shapes signalModel 	CatB dpWorkspace"+year+"_%d.root dpworkspace:signalModel_CatB	\n",i);
-		newcardShape << Form("shapes data_obs 		CatC dpWorkspace"+year+"_%d.root dpworkspace:data_obs_CatC	\n",i);
-		newcardShape << Form("shapes bkg_mass 		CatC dpWorkspace"+year+"_%d.root dpworkspace:bkg_mass_CatC	\n",i);
-		newcardShape << Form("shapes signalModel 	CatC dpWorkspace"+year+"_%d.root dpworkspace:signalModel_CatC	\n",i);
+		newcardShape << Form("shapes data_obs 		CatA dpWorkspace"+year+suff+"_%d.root dpworkspace:data_obs_CatA	\n",i);
+		newcardShape << Form("shapes bkg_mass 		CatA dpWorkspace"+year+suff+"_%d.root dpworkspace:bkg_mass_CatA	\n",i);
+		newcardShape << Form("shapes signalModel 	CatA dpWorkspace"+year+suff+"_%d.root dpworkspace:signalModel_CatA	\n",i);
+		newcardShape << Form("shapes data_obs 		CatB dpWorkspace"+year+suff+"_%d.root dpworkspace:data_obs_CatB	\n",i);
+		newcardShape << Form("shapes bkg_mass 		CatB dpWorkspace"+year+suff+"_%d.root dpworkspace:bkg_mass_CatB	\n",i);
+		newcardShape << Form("shapes signalModel 	CatB dpWorkspace"+year+suff+"_%d.root dpworkspace:signalModel_CatB	\n",i);
+		newcardShape << Form("shapes data_obs 		CatC dpWorkspace"+year+suff+"_%d.root dpworkspace:data_obs_CatC	\n",i);
+		newcardShape << Form("shapes bkg_mass 		CatC dpWorkspace"+year+suff+"_%d.root dpworkspace:bkg_mass_CatC	\n",i);
+		newcardShape << Form("shapes signalModel 	CatC dpWorkspace"+year+suff+"_%d.root dpworkspace:signalModel_CatC	\n",i);
       		newcardShape << Form("bin		CatA 	CatB	CatC\n");
       		newcardShape << Form("observation 	-1.0	-1.0	-1.0\n");
       		newcardShape << Form("bin     		CatA		CatA		CatB 		CatB		CatC		CatC	\n");
       		newcardShape << Form("process 		signalModel  	bkg_mass	signalModel  	bkg_mass	signalModel  	bkg_mass\n");
       		newcardShape << Form("process 		0    		1		0		1		0		1   	\n");
-      		newcardShape << Form("rate    		%f  		%f		%f  		%f		%f  		%f	\n",
+      		newcardShape << Form("rate    		%f  		%d		%f  		%d		%f  		%d	\n",
 		eps2scale*xsecgraph->Eval(mass)*eff*luminosity*acceptances[0],
-		catA->Integral(),
+		1,
 		eps2scale*xsecgraph->Eval(mass)*eff*luminosity*acceptances[1],
-		catB->Integral(),
+		1,
 		eps2scale*xsecgraph->Eval(mass)*eff*luminosity*acceptances[2],
-		catC->Integral());
+		1);
+		newcardShape << Form("bkg_norm_cata rateParam CatA bkg_mass %f\n",catA->Integral());
+		newcardShape << Form("bkg_norm_catb rateParam CatB bkg_mass %f\n",catB->Integral());
+		newcardShape << Form("bkg_norm_catc rateParam CatC bkg_mass %f\n",catC->Integral());
+		/*newcardShape << Form("a0a rateParam CatA bkg_mass %f\n"	       ,a0a.getValV());
+		newcardShape << Form("a0b rateParam CatB bkg_mass %f\n"	       ,a0b.getValV());
+		newcardShape << Form("a0c rateParam CatC bkg_mass %f\n"	       ,a0c.getValV());
+		newcardShape << Form("a1a rateParam CatA bkg_mass %f\n"	       ,a1a.getValV());
+		newcardShape << Form("a1b rateParam CatB bkg_mass %f\n"	       ,a1b.getValV());
+		newcardShape << Form("a1c rateParam CatC bkg_mass %f\n"	       ,a1c.getValV());
+		newcardShape << Form("a2a rateParam CatA bkg_mass %f\n"	       ,a2a.getValV());
+		newcardShape << Form("a2b rateParam CatB bkg_mass %f\n"	       ,a2b.getValV());
+		newcardShape << Form("a2c rateParam CatC bkg_mass %f\n"	       ,a2c.getValV());
+		newcardShape << Form("a3a rateParam CatA bkg_mass %f\n"	       ,a3a.getValV());
+		newcardShape << Form("a3b rateParam CatB bkg_mass %f\n"	       ,a3b.getValV());
+		newcardShape << Form("a3c rateParam CatC bkg_mass %f\n"	       ,a3c.getValV());*/
+
 		newcardShape.close();
 		
 	}
-
-
-
+	histChi2_A->SaveAs("histAchi2_"+year+".root");
+	histChi2_B->SaveAs("histBchi2_"+year+".root");
+	histChi2_C->SaveAs("histCchi2_"+year+".root");
 
 }
