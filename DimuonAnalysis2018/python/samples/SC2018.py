a0c9e740fbacd59f4f272e982abd7db7ca228f9f
Jakob <jakob.salfeld@cern.ch>
2018-12-13 21:55:20 +0100

    2018 analysis

---
+++ DimuonAnalysis2018/python/samples/SC2018.py
@@ -0,0 +1,89 @@
+samples = {}
+
+# Each sample has the following information
+# Dataset name
+# Collection of strings to be passed to cmsRun when running the config file (for VarParsing .. e.g. 'isMC=True')
+# JSON file in case of data 
+# Number of lumis / events to process
+
+def AddSCSamples(samples):
+
+    
+    samples['DarkPhotonToMuMu_M40']  = [
+        '/DarkPhotonToMuMu_M40_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+    samples['DarkPhotonToMuMu_M45']  = [
+        '/DarkPhotonToMuMu_M45_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+
+    samples['DarkPhotonToMuMu_M35']  = [
+        '/DarkPhotonToMuMu_M35_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+    samples['DarkPhotonToMuMu_M30']  = [
+        '/DarkPhotonToMuMu_M30_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+
+    samples['DarkPhotonToMuMu_M55']  = [
+        '/DarkPhotonToMuMu_M55_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+    samples['DarkPhotonToMuMu_M25']  = [
+        '/DarkPhotonToMuMu_M25_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+    
+    samples['DarkPhotonToMuMu_M15']  = [
+        '/DarkPhotonToMuMu_M15_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+
+    samples['DarkPhotonToMuMu_M10']  = [
+        '/DarkPhotonToMuMu_M10_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+    
+
+    samples['DarkPhotonToMuMu_M12p5']  = [
+        '/DarkPhotonToMuMu_M12p5_madgraph_pythia8_TuneCP5/RunIIAutumn18DRPremix-102X_upgrade2018_realistic_v15-v1/AODSIM',
+        ['isMC=True','xsec=1.'],
+        'EventAwareLumiBased',
+        '',
+        2000
+    ]
+    
+
+    
---
+++ DimuonAnalysis2018/python/samples/Samples2018.py
@@ -0,0 +1,7 @@
+samples = {}
+
+from DarkPhotonAnalysis.DimuonAnalysis2018.samples.SC2018 import AddSCSamples
+from DarkPhotonAnalysis.DimuonAnalysis2018.samples.DP import AddDPSamples
+
+AddSCSamples(samples)
+AddDPSamples(samples)
